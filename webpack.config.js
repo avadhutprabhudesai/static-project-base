aebf0806c1f311a4fb975460704cdea085e26284
Avadhut Prabhudesai <avadhutprabhudesai23@gmail.com>
2022-02-07 12:14:08 +0530

    refactor: #Remove SASS from project
    Removed SASS from project

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,8 @@ module.exports = {
         loader: 'html-loader',
       },
       {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.js/,
